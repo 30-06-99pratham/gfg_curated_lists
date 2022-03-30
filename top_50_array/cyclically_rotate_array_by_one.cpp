8d4c3daaeeb46034f0b9d85aec4a98e6a4e93ee1
Pratham Gaurav <61180816+30-06-99pratham@users.noreply.github.com>
2022-03-30 06:40:53 +0530

    Create cyclically_rotate_array_by_one.cpp

---
+++ top_50_array/cyclically_rotate_array_by_one.cpp
@@ -0,0 +1,9 @@
+// here the main key is to run loop from last element 
+// and do a[i] = a[i-1], since by ding this the a[i-1] will not be changed.
+
+// algo
+  // int temp = a[n-1]
+  // for(int i = n-1;i>0;i--){
+  //  a[i] = a[i-1]
+  //   }
+  // a[0] = temp;
